c9b561a5f9e5598c5cc0aa27864c6bda0fbec4fd
Nadesan <AF85896@ad.wellpoint.com>
2018-10-24 11:04:41 +0530

    Provider seach screen added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,9 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-
-export class AppComponent {
-
+export class AppComponent  {
+  constructor(private http: HttpClient){}
+  showBanner: boolean = true;
+  showTable: boolean = false;
   title = 'healcarepro365';
   providerName = '';
+  provider = null;
+  toggleForm($event) {
+    this.showBanner = !this.showBanner;
+  }
+
+  getProvider() {
+    this.http.get('http://localhost:8081/getProvider')
+     .subscribe(data => {
+      this.provider = data;
+       this.provider = this.provider.providerList;
+      this.showTable = true;
+      })
+     
+
+  }
 }
